069b4a7c9c100c5416aeec6be2207fe1a90fe6b3
alcenirdev08 <cenirdecarvalho@gmail.com>
2024-09-16 21:36:37 -0300

    atividade-aprofunda

---
+++ src/infrastructure/utils/id-generator.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IdentifierGenerator = void 0;
+class IdentifierGenerator {
+}
+exports.IdentifierGenerator = IdentifierGenerator;
